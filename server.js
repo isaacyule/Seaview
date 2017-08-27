bd513f12bf9eb9c532481d5397fb59eda6a81a49
Isaac Yule <isaac.k.yule@gmail.com>
2017-08-27 16:20:21 +0100

    added prices page, updated layout template

--- server.js
+++ server.js
@@ -11,11 +11,17 @@ app.set("view engine", ".handlebars");
 
 app.use(express.static(path.join(__dirname, "/public")));
 
+
 function homeHandler(request, response) {
 	response.render("home", {title: "Seaview Homepage"});
-}
+};
+
+function pricesHandler(request, response) {
+	response.render("prices", {title: "Seaview Prices"});
+};
 
 app.get("/", homeHandler);
+app.get("/prices", pricesHandler);
 
 app.listen(8080);
 
