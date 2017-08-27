0e180a8a48e6d473883889f91bd73b9dc8a8f432
Isaac Yule <isaac.k.yule@gmail.com>
2017-08-27 15:55:09 +0100

    added layout.handlebars
    added req/res handling in server.js
    added homepage

--- server.js
+++ server.js
@@ -1,1 +1,22 @@
 var express = require("express");
+var expressHandlebars = require("express-handlebars");
+var path = require("path");
+
+
+var app = express();
+
+app.engine(".handlebars", expressHandlebars({extname: ".handlebars", defaultLayout: "layout"}));
+
+app.set("view engine", ".handlebars");
+
+app.use(express.static(path.join(__dirname, "/public")));
+
+function homeHandler(req, res) {
+	response.render("home", {title: "Seaview Homepage"});
+}
+
+app.get("/", homeHandler);
+
+app.listen(8080);
+
+console.log("Hello cmd");
